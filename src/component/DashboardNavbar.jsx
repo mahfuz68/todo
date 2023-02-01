07d164d134ab3e798e8092ac484a48cd7d4b710f
MAHFUZ ANAM <mymsicret34@gmail.com>
2023-02-01 18:03:55 +0600

    fix icon

--- src/component/DashboardNavbar.jsx
+++ src/component/DashboardNavbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../assets/css/DashboardNavbar.css";
+import logoutIcon from "../assets/icon/logout.svg";
 import { useAuth } from "../context/AuthContext";
 
 export default function DashboardNavbar() {
@@ -105,10 +106,10 @@ export default function DashboardNavbar() {
             type="button"
             onClick={handleLogout}
           >
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
+              width="22"
+              height="22"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
@@ -120,7 +121,8 @@ export default function DashboardNavbar() {
               <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
               <polyline points="16 17 21 12 16 7"></polyline>
               <line x1="21" y1="12" x2="9" y2="12"></line>
-            </svg>
+            </svg> */}
+            <img width="20" height="20" src={logoutIcon} alt="" />
             Logout
           </button>
         </div>
